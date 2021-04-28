d07938177fbe4e52890e34b56108d2a008efc71f
montecamo <merrychristek@gmail.com>
2021-04-28 20:00:58 +0300

    feat: abstract users positions

---
+++ packages/ui/src/components/UserPositionProvider.vue
@@ -0,0 +1,78 @@
+<template>
+  <div
+    v-for="u in users"
+    class="user"
+    :key="u.id"
+    :style="{
+      left: `${u.position.left}px`,
+      top: `${u.position.top}px`,
+    }"
+  ></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject, ref, onMounted } from "vue";
+import { autorun } from "mobx";
+import { BehaviorSubject, combineLatest, Observable } from "rxjs";
+
+import { UsersStore, makeUser } from "../users";
+import type { FocusArea } from "../canvas";
+import type { Users } from "../users";
+import type { Api } from "../api";
+
+export default defineComponent({
+  inject: ["api"],
+  setup() {
+    const api = inject<Api>("api");
+    const focusArea$ = inject<Observable<FocusArea>>("focusArea$");
+    const scale$ = inject<Observable<number>>("scale$");
+
+    const store = new UsersStore();
+
+    const users = ref<Users>(store.users);
+
+    onMounted(() => {
+      if (api && focusArea$ && scale$) {
+        api.usersDisconnected$.subscribe((id) => {
+          store.deleteUser(id);
+        });
+
+        api.users$.subscribe((user) => {
+          store.deleteUser(user.id);
+          store.addUser(user);
+        });
+
+        const users$ = new BehaviorSubject<Users>(users.value);
+
+        autorun(() => {
+          users$.next(store.users);
+        });
+
+        combineLatest([users$, focusArea$, scale$]).subscribe(
+          ([serverUsers, { coordinates }, scale]) => {
+            users.value = serverUsers.map((u) =>
+              makeUser(u.id, {
+                left: (u.position.left - coordinates.x) * scale,
+                top: (u.position.top - coordinates.y) * scale,
+              })
+            );
+          }
+        );
+      }
+    });
+
+    return { users };
+  },
+});
+</script>
+
+<style scoped>
+.user {
+  position: fixed;
+
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: lightcoral;
+}
+</style>
