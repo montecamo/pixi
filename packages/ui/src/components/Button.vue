85accdfc68c72c55602f83881365242497fb0278
montecamo <merrychristek@gmail.com>
2021-04-28 20:01:12 +0300

    feat: add Button component

---
+++ packages/ui/src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" :class="{ [intent]: true, [classs]: true }">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: ["intent", "classs"],
+});
+</script>
+
+<style scoped>
+.button {
+  border-radius: 30px;
+  border: 0;
+  color: #fff;
+
+  padding: 15px 40px;
+  font-family: Quicksand;
+  font-size: 16px;
+}
+
+.primary {
+  background: var(--primary-color);
+  box-shadow: -15px 20px 40px rgba(253, 52, 110, 0.301961);
+}
+
+.secondary {
+  background: var(--secondary-color);
+  box-shadow: -15px 20px 40px rgba(96, 1, 211, 0.301961);
+}
+</style>
